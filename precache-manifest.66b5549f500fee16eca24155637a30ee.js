1f3ba822c6c07be171c1b6cb087a7db66e15cd80
Mark <Trrrrex93@gmail.com>
2020-06-27 17:28:43 +0400

    Updates

---
+++ precache-manifest.66b5549f500fee16eca24155637a30ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcb5542b47788deeaf3d659b8274921",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "dfb0dacb22c55f636f9e",
+    "url": "/weatherApp/static/css/main.14bd6cc9.chunk.css"
+  },
+  {
+    "revision": "4f175b4d7851d8ab1e72",
+    "url": "/weatherApp/static/js/2.36427db9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weatherApp/static/js/2.36427db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb0dacb22c55f636f9e",
+    "url": "/weatherApp/static/js/main.b63585c1.chunk.js"
+  },
+  {
+    "revision": "bcb830d94a724d76519b",
+    "url": "/weatherApp/static/js/runtime-main.c18d4ae8.js"
+  },
+  {
+    "revision": "6482ced03e9a3fc08398d65c9a9e8a6e",
+    "url": "/weatherApp/static/media/Andika-Regular.6482ced0.ttf"
+  },
+  {
+    "revision": "5f7f50f605c581bad2a835b1af0250be",
+    "url": "/weatherApp/static/media/temperature.5f7f50f6.png"
+  },
+  {
+    "revision": "be8e1db9e6e307c2ef778f61c02a50b9",
+    "url": "/weatherApp/static/media/wind.be8e1db9.png"
+  }
+]);
